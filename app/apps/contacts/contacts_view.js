0e461caddee6cae633f606f024c5a542e7e9f36b
michalkozminski <michal.kozminski@gmail.com>
2014-10-29 20:05:29 +0100

    Table added to modal

--- app/apps/contacts/contacts_view.js
+++ app/apps/contacts/contacts_view.js
@@ -1,14 +1,19 @@
-define(['application', 'hbs!apps/contacts/templates/contact'], function(Application, ContactTemplate){
+define(['application'
+      , 'hbs!apps/contacts/templates/contact'
+      , 'hbs!apps/contacts/templates/contacts_list'], function(Application, ContactTemplate, ContactsListTemplate){
   Application.module('ContactsApp.Views', function(Views, Application, Backbone, Marionette, $, _){
     Views.ConctactView = Marionette.ItemView.extend({
       template: ContactTemplate
+    , tagName: 'tr'
     , triggers: {
         'click': 'add'
       }
     });
 
-    Views.ListView = Marionette.CollectionView.extend({
-      childView: Views.ConctactView
+    Views.ListView = Marionette.CompositeView.extend({
+      template: ContactsListTemplate
+      , childView: Views.ConctactView
+      , childViewContainer: '.table'
     });
   });
   return Application.ContactsApp.Views;
